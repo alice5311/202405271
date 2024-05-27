4aae57afbf823e6a9b1c64fd147459bf965cfef5
alice5311 <150638195+alice5311@users.noreply.github.com>
2024-05-27 10:40:50 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -54,7 +54,7 @@ function draw() {
   image(cam, 0, 0);
 }
 
-function drawSkeleton() {
+function drawSkeleton() {  //偵測出各點並畫出線條
   // Draw all the tracked landmark points
   for (let i = 0; i < poses.length; i++) {
     pose = poses[i];
@@ -78,8 +78,27 @@ function drawSkeleton() {
     partB = pose.keypoints[12];
     if (partA.score > 0.1 && partB.score > 0.1) {
       line(partA.x, partA.y, partB.x, partB.y);
-      
     }
+    //eye
+    partL = pose.keypoints[1];
+    partR = pose.keypoints[12];
+    if (partL.score > 0.1 ) 
+    {
+      elipse(partL.x,partL.y,50)
+    }
+    if (partR.score > 0.1 ) 
+    {
+      elipse(partR.x,partR.y,50)
+    }
+    partA = pose.keypoints[0];
+    if(partA.score>0.1)
+    {
+      fill("#ff0000")
+      elipse(partA.x,partA.y,50)
+      fill("#ffffff")
+    }
+    
+    
     // shoulders to hips
     partA = pose.keypoints[5];
     partB = pose.keypoints[11];
